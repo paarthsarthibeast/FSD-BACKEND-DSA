fdb5103a00a5a168a0ce9ee2caec8333571f96ff
paarthsarthibeast <paarthsarthi18@gmail.com>
2025-02-07 11:54:55 +0530

    reading writing append fs

---
+++ Day1/p8.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+console.log('create new directory')
+
+const path = fs.mkdir('MyFolder',{recursive: true},(err)=>{
+    if(err){
+        console.log('an error occured',err);
+        return;
+    }
+    console.log("directory created")
+})
